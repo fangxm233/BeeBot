156aa419fe312db81f0062d258ec1bc08d4ccc47
fangxm <1126716733@qq.com>
2020-02-01 16:41:34 +0800

    调整缩进

--- src/profiler/screeps-profiler.js
+++ src/profiler/screeps-profiler.js
@@ -33,21 +33,21 @@ function setupProfiler() {
             const id = `id${Math.random()}`;
             /* eslint-disable */
             const download = `
-<script>
-  var element = document.getElementById('${id}');
-  if (!element) {
-    element = document.createElement('a');
-    element.setAttribute('id', '${id}');
-    element.setAttribute('href', 'data:text/plain;charset=utf-8,${encodeURIComponent(Profiler.callgrind())}');
-    element.setAttribute('download', 'callgrind.out.${Game.time}');
-  
-    element.style.display = 'none';
-    document.body.appendChild(element);
-  
-    element.click();
-  }
-</script>
-      `;
+                <script>
+                var element = document.getElementById('${id}');
+                if (!element) {
+                    element = document.createElement('a');
+                    element.setAttribute('id', '${id}');
+                    element.setAttribute('href', 'data:text/plain;charset=utf-8,${encodeURIComponent(Profiler.callgrind())}');
+                    element.setAttribute('download', 'callgrind.out.${Game.time}');
+                
+                    element.style.display = 'none';
+                    document.body.appendChild(element);
+                
+                    element.click();
+                }
+                </script>
+                    `;
             /* eslint-enable */
             console.log(download.split('\n').map((s) => s.trim()).join(''));
         },
