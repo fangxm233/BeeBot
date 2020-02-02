9fe09bcd5a658f8bef27b0b053e0903ac8e33d22
fangxm <1126716733@qq.com>
2020-02-02 17:25:34 +0800

    更改USER_NAME获取方式

--- src/config.ts
+++ src/config.ts
@@ -1,4 +1,6 @@
+import { getUsername } from "utilities/utils";
+
 export const USE_PROFILE = false;
 export const USE_ACTION_COUNTER = true;
 
-export const USER_NAME = 'fangxm';
+export const USER_NAME = getUsername();
