b1ed0b4866e7ba89b74b779a4eedba121d06a71a
fangxm <1126716733@QQ.COM>
2020-10-28 23:27:23 +0800

    feat: init Memory, using same memory

--- src/memory/Memory.ts
+++ src/memory/Memory.ts
@@ -1,10 +1,37 @@
-export class Men {
-    public static cleanCreeps() {
-        // 清理不存在的creep的内存
-        for (const name in Memory.creeps) {
-			if (!Game.creeps[name]) {
-				delete Memory.creeps[name];
-			}
-		}
+export const MemoryVersion = 1;
+
+export class Mem {
+    public static checkAndInit() {
+        if (!Memory.MemVer) this.initMemory();
+    }
+
+    public static initMemory() {
+        delete (global as any).Memory;
+        (global as any).Memory = {};
+        (RawMemory as any)._parsed = (global as any).Memory;
+        Memory.MemVer = MemoryVersion;
+
+        Memory.processes = {};
+    }
+
+    public static tryInitSameMemory(): boolean {
+        if (global.lastMemoryTick && global.LastMemory && Game.time == (global.lastMemoryTick + 1)) {
+            delete global.Memory;
+            global.Memory = global.LastMemory;
+            (RawMemory as any)._parsed = global.LastMemory;
+            global.lastMemoryTick = Game.time;
+            return true;
+        } else {
+            // tslint:disable-next-line: no-unused-expression
+            Memory.rooms;
+            global.LastMemory = (RawMemory as any)._parsed;
+            global.lastMemoryTick = Game.time;
+            return false;
+        }
+    }
+
+    // tslint:disable-next-line: no-empty
+    private static upgradeMemory(from: number, to: number) {
+
     }
 }
