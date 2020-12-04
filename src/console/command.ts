11bcbb48d3f7ef659592725480e4229e1cd1531f
fangxm <1126716733@QQ.COM>
2020-12-04 19:08:46 +0800

    feat: add cancelOutpost to command

--- src/console/command.ts
+++ src/console/command.ts
@@ -4,6 +4,17 @@ export class Command {
     public static run() {
         for (const name in Game.flags) {
             const flag = Game.flags[name];
+            if (flag.name == 'cancelOutpost') {
+                const to = flag.pos.roomName;
+                for (const roomName in Memory.beebot.outposts) {
+                    const outposts = Memory.beebot.outposts[roomName];
+                    if (_.contains(outposts, to)) {
+                        BeeBot.cancelOutpost(roomName, to);
+                    }
+                }
+                flag.remove();
+                continue;
+            }
             if (flag.name.match('outpost')) {
                 const from = flag.name.split('_')[1];
                 if (from) BeeBot.goOutpost(from, flag.pos.roomName);
