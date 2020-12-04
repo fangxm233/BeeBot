a491afb02358502890158b201f1e70316f1440be
fangxm <1126716733@QQ.COM>
2020-12-04 18:56:40 +0800

    feat: fetch nearby rooms' energy

--- src/resourceManagement/ResourcesManager.ts
+++ src/resourceManagement/ResourcesManager.ts
@@ -1,5 +1,6 @@
 import { RoomPlanner } from "basePlanner/RoomPlanner";
 import { Bee } from "Bee/Bee";
+import { BeeBot } from "BeeBot/BeeBot";
 import { profile } from "profiler/decorator";
 import { withdrawTargetType } from "tasks/instances/task_withdraw";
 import { Task } from "tasks/Task";
@@ -9,11 +10,20 @@ import { Tasks } from "tasks/Tasks";
 export class ResourcesManager {
 
     public static getEnergySource(bee: Bee, isFiller: boolean, minAmount?: number): Task | null {
+        const early = !Game.rooms[bee.process.roomName].storage;
         const remain = minAmount === undefined ? bee.store.getFreeCapacity() * 0.5 : minAmount;
-        const candidates: (withdrawTargetType | Resource)[] =
+
+        let candidates: (withdrawTargetType | Resource)[] =
             bee.room.tombstones.filter(tomb => tomb.store.energy >= remain);
         candidates.push(...bee.room.droppedEnergy.filter(energy => energy.amount > remain));
         candidates.push(...bee.room.ruins.filter(ruin => ruin.store.energy >= remain));
+        if (early) {
+            BeeBot.getEarlyOutposts(bee.process.roomName).forEach(roomName => {
+                const room = Game.rooms[roomName];
+                if (!room) return;
+                candidates.push(...room.droppedEnergy.filter(energy => energy.amount > remain));
+            })
+        }
 
         if (bee.room.storage) {
             if (bee.room.storage.energy >= remain) candidates.push(bee.room.storage);
@@ -31,6 +41,14 @@ export class ResourcesManager {
         if (spawnRoom?.storage && spawnRoom.storage.store.energy >= remain) {
             candidates.push(spawnRoom.storage);
         }
+
+        candidates = candidates.filter(structure => {
+            function getOrdered(target: withdrawTargetType | Resource) {
+                return _.sum(target.targetedBy, creep => creep.store.getFreeCapacity());
+            }
+            if (structure instanceof Resource) return structure.amount > getOrdered(structure);
+            return structure.store.energy >= getOrdered(structure);
+        });
 
         if (candidates.length) {
             const target = bee.pos.findClosestByMultiRoomRange(candidates);
