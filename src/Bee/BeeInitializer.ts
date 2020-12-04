37f8b8796d07274a21ab403519d9028620831f06
fangxm <1126716733@QQ.COM>
2020-12-04 19:04:34 +0800

    refact: change priority

--- src/Bee/BeeInitializer.ts
+++ src/Bee/BeeInitializer.ts
@@ -11,9 +11,9 @@ import { BeeWorker } from "./instances/worker";
 
 // 注册顺序将会决定role的优先级 先注册的初始优先级高
 BeeFactorty.registerBee(ROLE_FILLER, BeeFiller);
+BeeFactorty.registerBee(ROLE_MANAGER, BeeManager);
 BeeFactorty.registerBee(ROLE_MINER, BeeMiner);
 BeeFactorty.registerBee(ROLE_CARRIER, BeeCarrier);
-BeeFactorty.registerBee(ROLE_MANAGER, BeeManager);
 BeeFactorty.registerBee(ROLE_WORKER, BeeWorker);
 BeeFactorty.registerBee(ROLE_UPGRADER, BeeUpgrader);
 BeeFactorty.registerBee(ROLE_SCOUT, BeeScout);
