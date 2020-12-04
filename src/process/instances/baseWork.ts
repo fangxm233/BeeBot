a034cdf8f61e2db83797391f7260c41d8643d294
fangxm <1126716733@QQ.COM>
2020-12-04 19:10:38 +0800

    feat: add manager logic to baseWork

--- src/process/instances/baseWork.ts
+++ src/process/instances/baseWork.ts
@@ -1,15 +1,29 @@
+import { ROAD_CONSTRUCT_RCL } from "basePlanner/BaseConstructor";
+import { RoomPlanner } from "basePlanner/RoomPlanner";
+import { BeeBot } from "BeeBot/BeeBot";
 import { setups } from "beeSpawning/setups";
 import { WishConfig, WishManager } from "beeSpawning/WishManager";
-import { ROLE_WORKER } from "declarations/constantsExport";
+import { PROCESS_MINE_SOURCE, ROLE_MANAGER, ROLE_WORKER } from "declarations/constantsExport";
 import { PROCESS_BASE_WORK } from "declarations/constantsExport";
+import { event } from "event/Event";
 import { Process } from "process/Process"
 import { profile } from "profiler/decorator";
+import { ProcessMineSource } from "./mineSource";
 
 @profile
 export class ProcessBaseWork extends Process {
+    private maxWorkerCount: number;
+    private spawnManager: boolean;
+    private workerConfig: WishConfig;
+    private managerConfig: WishConfig;
+    private inited: boolean;
+
     constructor(roomName: string) {
         super(roomName, PROCESS_BASE_WORK);
         this.wishManager = new WishManager(roomName, roomName, this);
+        this.wishManager.setDefault('budget', Infinity);
+
+        event.addEventListener('onRclUpgrade', () => this.init())
     }
 
     public static getInstance(proto: protoProcess, roomName: string) {
@@ -18,9 +32,45 @@ export class ProcessBaseWork extends Process {
 
     public run() {
         this.foreachBee(ROLE_WORKER, bee => bee.run());
+        this.foreachBee(ROLE_MANAGER, bee => bee.run());
+    }
+
+    private chooseSetup(): boolean {
+        const room = Game.rooms[this.roomName];
+        if (!room) return false;
+        if (room.controller!.level >= ROAD_CONSTRUCT_RCL) {
+            this.workerConfig.setup = setups[ROLE_WORKER].default;
+        } else {
+            this.workerConfig.setup = setups[ROLE_WORKER].early;
+        }
+
+        return true;
+    }
+
+    private judgeMaxCount() {
+        const processes = Process.getProcesses<ProcessMineSource>(this.roomName, PROCESS_MINE_SOURCE);
+        return this.maxWorkerCount = _.sum(processes, process => process.sources?.length) * 5;
+    }
+
+    private init(): boolean {
+        const room = Game.rooms[this.roomName];
+        if (!room) return false;
+
+        this.workerConfig = { role: ROLE_WORKER };
+        this.managerConfig = { role: ROLE_MANAGER, setup: setups[ROLE_MANAGER].default } // 容量1500
+
+        if (room.controller!.level > 4) {
+            this.spawnManager = true;
+        }
+
+        this.judgeMaxCount();
+
+        return this.chooseSetup();
     }
 
     public wishCreeps() {
+        if (!this.inited && !this.init()) return;
+
         const room = Game.rooms[this.roomName];
         const controller = room?.controller;
         if (!room || !controller) {
@@ -28,10 +78,18 @@ export class ProcessBaseWork extends Process {
             return;
         }
 
+        this.wishManager.clear();
+
         let bodies: BodyPartConstant[] | undefined = undefined;
-        bodies = setups[ROLE_WORKER].early.generateCreepBody(room.energyCapacityAvailable);
-        const MAX_WORKERS = 10;
+        bodies = this.workerConfig.setup!.generateCreepBody(room.energyCapacityAvailable);
+        const MAX_WORKERS = this.maxWorkerCount;
         const buildSites = room.find(FIND_CONSTRUCTION_SITES);
+        BeeBot.getOutposts(room.name).forEach(roomName => {
+            const data = RoomPlanner.getRoomData(roomName);
+            if (!data) return;
+            buildSites.push(..._.filter(Game.constructionSites,
+                site => _.find(data.sourcesPath, path => path.path.find(pos => site.pos.isEqualTo(pos)))));
+        });
         const buildTicks = _.sum(buildSites,
             site => Math.max(site.progressTotal - site.progress, 0)) / BUILD_POWER;
         let numWorkers = Math.ceil(2 * (5 * buildTicks) /
@@ -40,9 +98,14 @@ export class ProcessBaseWork extends Process {
         if (controller.level < 4) numWorkers = Math.max(numWorkers, MAX_WORKERS);
 
         const nowCount = this.getCreepAndWishCount(ROLE_WORKER);
-        const config: WishConfig = { role: ROLE_WORKER, setup: setups[ROLE_WORKER].default, budget: Infinity, count: numWorkers - nowCount }
         if (nowCount < numWorkers) {
-            this.wishManager.wishBee(config);
+            this.wishManager.wishBee(_.assign(_.clone(this.workerConfig), { count: numWorkers - nowCount }));
+        }
+
+        if (this.spawnManager) {
+            this.wishManager.arrangeCyclingBees(ROLE_MANAGER, this.managerConfig.setup!, this.managerConfig.budget!);
+            const nowCount = this.getCreepAndWishCount(ROLE_MANAGER);
+            if (nowCount < 1) this.wishManager.wishBee(this.managerConfig);
         }
     }
 }
