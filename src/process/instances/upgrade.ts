06f651d884f19f3c9a243ed8d2a863a9bc9e798c
fangxm <1126716733@QQ.COM>
2020-12-04 19:09:54 +0800

    feat: complete upgrade process

--- src/process/instances/upgrade.ts
+++ src/process/instances/upgrade.ts
@@ -1,15 +1,30 @@
+import { BeeSetup } from "beeSpawning/BeeSetup";
 import { setups } from "beeSpawning/setups";
-import { WishConfig, WishManager } from "beeSpawning/WishManager";
+import { WishManager } from "beeSpawning/WishManager";
 import { ROLE_UPGRADER } from "declarations/constantsExport";
 import { PROCESS_UPGRADE } from "declarations/constantsExport";
+import { event } from "event/Event";
 import { Process } from "process/Process";
 import { profile } from "profiler/decorator";
 
+const START_UPGRADE_LINE = 3e5;
+const UPGRADER_POW = 1.5;
+const UPGRADER_BIAS = -3;
+const MAX_UPGRADER = 10;
+
 @profile
 export class ProcessUpgrade extends Process {
+    private setup: BeeSetup;
+    private count: number;
+    private inited: boolean;
+
     constructor(roomName: string) {
         super(roomName, PROCESS_UPGRADE);
         this.wishManager = new WishManager(roomName, roomName, this);
+        this.wishManager.setDefault('role', ROLE_UPGRADER);
+        this.wishManager.setDefault('budget', Infinity);
+
+        event.addEventListener('onRclUpgrade', () => this.init());
     }
 
     public static getInstance(proto: protoProcess, roomName: string) {
@@ -17,14 +32,51 @@ export class ProcessUpgrade extends Process {
     }
 
     public run() {
+        if (!this.inited && !this.init()) return;
         this.foreachBee(ROLE_UPGRADER, bee => bee.run());
     }
 
+    private init(): boolean {
+        if (!this.chooseSetup() || !this.judgeCount()) return false;
+        return this.inited = true;
+    }
+
+    private chooseSetup(): boolean {
+        const room = Game.rooms[this.roomName];
+        if (!room) return false;
+        const level = room.controller!.level;
+        if (level < 4) return false;
+        if (level == 8) this.setup = setups[ROLE_UPGRADER].final;
+        else if (room.controller!.pos.findInRange(FIND_STRUCTURES, 2).filter(
+            structure => structure.structureType == STRUCTURE_LINK).length) this.setup = setups[ROLE_UPGRADER].heavy;
+        else this.setup = setups[ROLE_UPGRADER].default;
+
+        return true;
+    }
+
+    private judgeCount(): boolean {
+        const room = Game.rooms[this.roomName];
+        if (!room) return false;
+        const level = room.controller!.level;
+        if (level < 4) return false;
+        const storage = room.storage;
+        if (!storage || storage.store.energy < START_UPGRADE_LINE) {
+            this.count = 0;
+            return true;
+        }
+
+        this.count = Math.min(Math.round(
+            Math.pow(storage.store.energy / 1e5, UPGRADER_POW)) + UPGRADER_BIAS, MAX_UPGRADER);
+
+        return true;
+    }
+
     public wishCreeps() {
+        if (!this.inited && !this.init()) return;
+
         const nowCount = this.getCreepAndWishCount(ROLE_UPGRADER);
-        const count = 2;
-        const config: WishConfig = { role: ROLE_UPGRADER, setup: setups[ROLE_UPGRADER].default, budget: Infinity, count: count - nowCount }
-        if (nowCount < count) {
+        const config = { setup: this.setup, count: this.count - nowCount };
+        if (nowCount < this.count) {
             this.wishManager.wishBee(config);
         }
     }
