3e4a39621ba7bebe9629097704b3182f74fd0614
fangxm <1126716733@QQ.COM>
2020-12-13 14:12:43 +0800

    feat: cancel genetrating pixel

--- src/sandBox.ts
+++ src/sandBox.ts
@@ -1,6 +1,6 @@
 export function sandBox() {
     // @ts-ignore
-    if (Game.cpu.generatePixel && Game.cpu.bucket == 10000) {
-        Game.cpu.generatePixel();
-    }
+    // if (Game.cpu.generatePixel && Game.cpu.bucket == 10000) {
+    //     Game.cpu.generatePixel();
+    // }
 }
