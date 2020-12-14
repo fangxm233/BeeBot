5743017cf4ed06bbb72725af69e60d7146822f8c
fangxm <1126716733@QQ.COM>
2020-12-14 20:03:51 +0800

    refact: trace error to impove debug

--- src/process/Processes.ts
+++ src/process/Processes.ts
@@ -81,6 +81,7 @@ export class Processes {
                     }
                 } catch (e) {
                     log.throw(e);
+                    log.trace(e);
                 }
             });
         }
