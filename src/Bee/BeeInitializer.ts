3ed97bfb271d37d62250f7fcc620966bdf022227
fangxm <1126716733@QQ.COM>
2020-12-14 20:05:34 +0800

    fix: missed commit

--- src/Bee/BeeInitializer.ts
+++ src/Bee/BeeInitializer.ts
@@ -2,7 +2,7 @@ import { BeeClaimer } from 'Bee/instances/claimer';
 import { BeePioneer } from 'Bee/instances/pioneer';
 import {
     ROLE_CARRIER,
-    ROLE_CLAIMER,
+    ROLE_CLAIMER, ROLE_DE_INVADER, ROLE_DE_INVADER_CORE,
     ROLE_FILLER,
     ROLE_MANAGER,
     ROLE_MINER,
@@ -21,12 +21,16 @@ import { BeeReserver } from './instances/reserver';
 import { BeeScout } from './instances/scout';
 import { BeeUpgrader } from './instances/upgrader';
 import { BeeWorker } from './instances/worker';
+import { BeeDefendInvader } from 'Bee/instances/defendInvader';
+import { BeeDefendInvaderCore } from 'Bee/instances/defendInvaderCore';
 
 // 注册顺序将会决定role的优先级 先注册的初始优先级高
 BeeFactorty.registerBee(ROLE_FILLER, BeeFiller);
 BeeFactorty.registerBee(ROLE_MANAGER, BeeManager);
 BeeFactorty.registerBee(ROLE_CLAIMER, BeeClaimer);
 BeeFactorty.registerBee(ROLE_PIONEER, BeePioneer);
+BeeFactorty.registerBee(ROLE_DE_INVADER, BeeDefendInvader);
+BeeFactorty.registerBee(ROLE_DE_INVADER_CORE, BeeDefendInvaderCore);
 BeeFactorty.registerBee(ROLE_MINER, BeeMiner);
 BeeFactorty.registerBee(ROLE_CARRIER, BeeCarrier);
 BeeFactorty.registerBee(ROLE_WORKER, BeeWorker);
