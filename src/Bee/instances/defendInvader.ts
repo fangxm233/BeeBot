b7dd9e592453607c7f530dd21476e955b7bd1aba
fangxm <1126716733@QQ.COM>
2020-12-22 22:01:33 +0800

    fix: heal self when hit lost & ranged heal

--- src/Bee/instances/defendInvader.ts
+++ src/Bee/instances/defendInvader.ts
@@ -7,6 +7,8 @@ export class BeeDefendInvader extends Bee {
     public process: ProcessDefendInvader;
 
     protected runCore(): number | void {
+        if (this.hitsLost) this.heal(this.creep);
+
         if (this.room.name != this.process.target) {
             this.travelToRoom(this.process.target);
             return;
@@ -14,7 +16,6 @@ export class BeeDefendInvader extends Bee {
 
         const invaders = this.room.find(FIND_HOSTILE_CREEPS);
         if (invaders.length) {
-            this.heal(this.creep);
             const invader = this.pos.findClosestByRange(invaders);
             if (this.pos.inRangeTo(invader!, 3)) this.rangedAttack(invader!);
             else this.travelTo(invader!);
@@ -28,8 +29,8 @@ export class BeeDefendInvader extends Bee {
             const injures = this.room.find(FIND_MY_CREEPS).filter(creep => creep.hitsLost);
             const injure = this.pos.findClosestByRange(injures);
             if (injure) {
-                if (this.pos.isNearTo(injure)) this.heal(injure);
-                else this.travelTo(injure);
+                this.heal(injure);
+                if(!this.pos.isNearTo(injure)) this.travelTo(injure);
                 return;
             }
         }
